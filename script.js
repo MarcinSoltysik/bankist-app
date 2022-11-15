930a8c8fbda60874beb52882024d01b022a07a02
Marcin Soltysik <profittax@gmail.com>
2022-11-15 23:10:09 +0100

    feat: 003 Add function calcDisplaySummary

--- script.js
+++ script.js
@@ -97,10 +97,7 @@ const calcDisplaySummary = function (movements) {
   const interest = movements
     .filter(mov => mov > 0)
     .map(deposit => (deposit * 1.2) / 100)
-    .filter((int, i, arr) => {
-      console.log(arr);
-      return int > 1;
-    })
+    .filter(int => int > 1)
     .reduce((acc, int) => acc + int, 0);
   labelSumInterest.textContent = `${interest}€`;
 };
